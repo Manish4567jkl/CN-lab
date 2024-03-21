0068e3ad5431b2696cb19b7876cc0e58fb8699cd
Manish4567jkl <manishmandal895@gmail.com>
2024-03-21 10:11:56 +0530

    decrypt

--- encryption.c
+++ encryption.c
@@ -1,42 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 int main() {
     char message[30], ch;
     int i, key;
-    
     printf("Enter a message to encrypt: ");
     scanf("%s", message);
-    
     printf("Enter key: ");
     scanf("%d", &key);
-    
     for (i = 0; message[i] != '\0'; i++) {
         ch = message[i];
-        
         if (ch >= 'a' && ch <= 'z') {
             ch = ch + key;
             
             if (ch > 'z') {
                 ch = ch - 'z' + 'a' - 1;
             }
-            
             message[i] = ch;
         }
         else if (ch >= 'A' && ch <= 'Z') {
             ch = ch + key;
-            
             if (ch > 'Z') {
-                ch = ch - 'Z' + 'A' - 1;
-            }
-            
+                ch = ch - 'Z' + 'A' - 1;   
             message[i] = ch;
         }
-      
-      
-    }
-    
+    }    
     printf("Encrypted message: %s\n", message);
-    
     return 0;
 }
