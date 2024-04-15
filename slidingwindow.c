74e49af66f19a72aa6c22c3db71cc9aea3664d48
Manish4567jkl <manishmandal895@gmail.com>
2024-04-15 19:29:28 +0530

    final commit

--- slidingwindow.c
+++ slidingwindow.c
@@ -1,28 +1,41 @@
 #include<stdio.h>
+#include<stdlib.h>
+
 void main(){
-    int w,f,frames[30],i;
-    printf("Enter window size :");
-    scanf("%d",&w);
-    printf("Enter the number of frames to transmit");
-    scanf("%d",&f);
-    printf("Enter %d frames" , f);
+    int w,f;
+   int frame[50];
+   int i,j;
 
-    for(i=0;i<=f;i++){
-        scanf("%d",&frames[i]);
-    }
-    printf("\n With sliding window protocol the frames will be sent in the following manner ");
-    printf("After sending %d frames at each stage , sender wait for ack send by reciever \n\n" , w);
+   printf("enter window size :");
+   scanf("%d",&w);
 
-    for(i=1;i<=frames;i++){
-        if(i%w == 0){
-            printf("%d\n" , frames[i]);
-            printf("Acknowledgement of the above frame sent is recieved by reciever");
-        }
-        else{
-            printf("%d",frames[i]);
-        }
-        if(f%w != 0){
-            printf("Acknowledgement of above frame sent is recieved by the sender");
-        }
+   printf("Enter Number of frames to transmit :");
+   scanf("%d",&f);
+
+   printf("Enter %d frames :",f);
+
+   for(i=1;i<=f;i++){
+    scanf("%d",&frame[i]);
+   }
+
+   printf("\nWith sliding window protocol the frames will be sent in following manner\n");
+
+   printf("\nAfter sending %d frames at each stage sender wait for ack send by reciever\n\n",w);
+
+   for(i=1;i<=f;i++){
+    if(i % w == 0){
+        printf("%d\n",frame[i]);
+        printf("Acknowledgement of above frame sent is recieved by sender\n");
+        sleep(2);
     }
-}
+    else{
+        printf("%d\n",frame[i]);
+    }
+    if(f % w != 0){
+        printf("\nAcknowledgement of above frame sent is recieved by sender\n");
+    }
+   }
+    
+     }
+
+ 
